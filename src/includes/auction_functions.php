d056e0920d22e12d41fb4f538f5469cf4cd74354
Sadiq Sahib <ucabsjs@ucl.ac.uk>
2016-02-16 00:16:08 +0000

    Connected the auction_list.php with auction_view.php, the auction selected will now be displayed on a separate view where a buyer will be able to bid

--- src/includes/auction_functions.php
+++ src/includes/auction_functions.php
@@ -20,6 +20,17 @@
   return $item_set;
  }
 
+function find_auction_for_chosen_item($itemID) {
+    global $connection;
+    $query = "SELECT * ";
+    $query .= "FROM Auction ";
+    $query .= "WHERE itemID = {$itemID} ";
+    $query .= "LIMIT 1";
+    $item_set = mysqli_query($connection,$query);
+    confirm_query($item_set);
+    return $item_set;
+}
+
  function confirm_query($result_set) {
   if (!$result_set) {
    die("Database query failed.");
