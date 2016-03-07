067ac938dde421a4d1db26c2bc933569a6e5e563
Aditya Pradhan <apradhan@live.co.uk>
2016-03-07 22:18:28 +0000

    Refactored watch_list.php page
    1. header, footer and navbar html moved into includes/layouts folder to be used
    as reuseable templates

---
+++ src/includes/layouts/navbar.php
@@ -0,0 +1,49 @@
+<!-- Navigation bar-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Auction vault</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <div class="floatleft"></div>
+                <li>
+                    <a href="#">My Auctions</a>
+                </li>
+                <li>
+                    <a href="#">My Bids</a>
+                </li>
+                <li>
+                    <a href="watch_list.php">Watch-list</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact us</a>
+                </li>
+                <li>
+                    <a href="#">About us</a>
+                </li>
+                <?php   //This long repetitive line is to align the Logout button far right lol XD ?>
+                </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li>
+
+
+                <li>
+                    <a href="loginPage.php">Log out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
