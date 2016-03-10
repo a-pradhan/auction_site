367338b98f1113c0ccb980c4fd4b3322c2967e55
Aditya Pradhan <apradhan@live.co.uk>
2016-03-10 16:19:57 +0000

    All remaining occurrences of admin_id SESSION variable key were changed to userID

--- src/public/auction_list.php
+++ src/public/auction_list.php
@@ -9,90 +9,12 @@
 <?php
     $username = $_SESSION["username"];
     $password = $_SESSION["password"];
-    $loggedIn_userID = $_SESSION["admin_id"];
+    $loggedIn_userID = $_SESSION["userID"];
 ?>
 
+<?php require("../includes/layouts/header.php"); ?>
+<?php require("../includes/layouts/navbar.php"); ?>
 
-<html lang="en">
-<head>
-    <title>Auction Vault</title>
-</head>
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>1 Col Portfolio - Start Bootstrap Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../css/1-col-portfolio.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="auction_list.php">Auction Vault</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <div class="floatleft"></div>
-                    <li>
-                        <a href="my_auctions.php">My Auctions</a>
-                    </li>
-                    <li>
-                        <a href="my_bids.php">My Bids</a>
-                    </li>
-                    <li>
-                        <a href="watch_list.php">Watch-list</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact us</a>
-                    </li>
-                    <li>
-                        <a href="#">About us</a>
-                    </li>
-                    <?php   //This long repetitive line is to align the Logout button far right lol XD ?>
-                     </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a> </li><li> <a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li><li><a href="#"></a></li>
-
-
-                    <li>
-                        <a href="loginPage.php">Log out</a>
-                    </li>
-                </ul>
-                </div>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
 
 <!-- Page Content -->
 <div class="container">
