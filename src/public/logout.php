dcb0ceee82db6e7bb5bdbd075f4bbbcf8edaffda
Aditya Pradhan <apradhan@live.co.uk>
2016-03-10 17:48:46 +0000

    implemented logout that completely deletes the SESSION and COOKIE variables which were causing conflicting behaviour when logging out and loggin in as a different user
    1. logout processing is done on the logout.php file

---
+++ src/public/logout.php
@@ -0,0 +1,16 @@
+<?php
+// completely destroy the session
+
+// set session to empty array
+$_SESSION = array();
+
+// if cookie with that session name exists make it expire
+if (isset($_COOKIE[session_name()])) {
+setcookie(session_name(), '', time() - 42000, '/');
+}
+
+// destroys the session file on the server
+session_destroy();
+
+redirect_to("loginPage.php");
+?>
